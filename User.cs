b3432cf71322e8ca4cda137ea48e9bea32e43599
theoneandholymoses <121929370+theoneandholymoses@users.noreply.github.com>
2025-01-15 16:44:39 +0200

    Update User.cs

--- User.cs
+++ User.cs
@@ -9,18 +9,27 @@ namespace testTinderDogs
 {
     public class User
     {
-        public string id {  get; set; } = Guid.NewGuid().ToString();
+        public string id { get; set; } = Guid.NewGuid().ToString();
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Required, EmailAddress]
         public string Email { get; set; }
+        [Phone]
         public string Phone { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
         public DateTime BirthDate { get; set; }
-        public bool Gender { get; set; }
-        public bool IsAdmin { get; set; }
-        public List<Dog> Dogs { get; set; }
-        // public string google_id(nullable for email login)
+        public Gender Gender { get; set; }
+        public bool IsAdmin { get; set; } = false;
+        public List<Dog> Dogs { get; set; } = new();
+        public string googleId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+    }
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
     }
 }
